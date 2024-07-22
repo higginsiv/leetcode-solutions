4435c759d184e17b0806d1091f601a104776b603
William Higgins <higginsiv@gmail.com>
2024-07-22 18:49:12 -0400

    0041 lc complete

--- 0041/0041.js
+++ 0041/0041.js
@@ -2,12 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-const firstMissingPositive = function(nums) {
-   const length = nums.length;
-   for (let i = 1; i <= length; i++) {
-       if (!nums.includes(i)) {
-           return i;
-       }
-   }
-   return length + 1;
+const firstMissingPositive = function (nums) {
+    const length = nums.length;
+    let i = 0;
+
+    while (i < length) {
+        const num = nums[i];
+
+        if (nums[num - 1] !== num && num > 0 && num <= length) {
+            [nums[i], nums[num - 1]] = [nums[num - 1], nums[i]];
+        } else {
+            i++;
+        }
+    }
+    
+    for (let i = 1; i <= length; i++) {
+        if (nums[i - 1] !== i) {
+            return i;
+        }
+    }
+    return length + 1;
 };
