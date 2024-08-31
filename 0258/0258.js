0ec4e707f96593e42f78d3ac9b5b532294d495d9
William Higgins <higginsiv@gmail.com>
2024-08-31 19:42:40 -0400

    258 add comment to source of math

--- 0258/0258.js
+++ 0258/0258.js
@@ -2,6 +2,7 @@
  * @param {number} num
  * @return {number}
  */
+// https://en.wikipedia.org/wiki/Digital_root
 const addDigits = function (num) {
     return num === 0 ? 0 : 1 + ((num - 1) % 9);
 };
