64cd6b31c870601c63dddb50ef97188a3590402e
William Higgins <higginsiv@gmail.com>
2024-09-05 20:10:21 -0400

    57 finish

--- 0057/0057.js
+++ 0057/0057.js
@@ -4,12 +4,16 @@
  * @return {number[][]}
  */
 const insert = function (intervals, newInterval) {
+    if (!intervals || intervals.length === 0) {
+        return [newInterval];
+    }
     const [newStart, newEnd] = newInterval;
     let high = intervals.length - 1;
     let low = 0;
     let mid;
     let insertionPoint;
 
+    // find insertion point in array
     while (low <= high) {
         mid = Math.floor((high + low) / 2);
         const [start, end] = intervals[mid];
@@ -21,7 +25,6 @@ const insert = function (intervals, newInterval) {
         } 
 
         if (start <= newStart && ((nextStart > newStart) || (nextStart == null))) {
-            console.log('golden')
             mid = mid + 1;
             break;
         } else if (start <= newStart) {
@@ -32,36 +35,35 @@ const insert = function (intervals, newInterval) {
     }
     insertionPoint = mid;
     intervals.splice(insertionPoint, 0, newInterval);
-    let merged = [];
-    let i = insertionPoint - 1 >= 0 ? insertionPoint - 1 : 0;
+
+    // Get all overlap to the right
     while (true) {
-        if (i = intervals.length - 1) {
+        if (insertionPoint === intervals.length - 1) {
             break;
         }
 
-        let [currentStart, currentEnd] = intervals[i];
-        let [nextStart, nextEnd] = intervals[i + 1];
+        let [currentStart, currentEnd] = intervals[insertionPoint];
+        let [nextStart, nextEnd] = intervals[insertionPoint + 1];
 
-        // determine overlap
         if (currentStart <= nextStart && currentEnd >= nextStart) {
-            // overlap
-            if (!merged[0]) {
-                merged[0] = currentStart;
-            }
-            i++;
+            newInterval[1] = Math.max(newInterval[1], nextEnd);
+            intervals.splice(insertionPoint + 1, 1);
         } else {
             break;
         }
     }
 
-    console.log(mid);
-    console.log(insertionPoint)
-    console.log(intervals)
+    // Get potential overlap to the left, which can at most be one element
+    if (insertionPoint != 0) {
+        let [priorStart, priorEnd] = intervals[insertionPoint - 1];
+        if (newInterval[0] >= priorStart && newInterval[0] <= priorEnd) {
+            newInterval[0] = priorStart;
+            if (newInterval[1] >= priorStart && newInterval[1] <= priorEnd) {
+                newInterval[1] = priorEnd
+            }
+            intervals.splice(insertionPoint - 1, 1);
+        }
+    }
+
+    return intervals;
 };
-
-let intervals = [[1,3],[6,9]], newInterval = [2,5];
-// let intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8];
-// let intervals = [[3,6]], newInterval = [2,5];
-
-insert(intervals, newInterval);
-// 1,3 4,5
