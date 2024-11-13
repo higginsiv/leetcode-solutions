0cfeb9be9f893bf1465b760aadd512fbcabafe4f
William Higgins <higginsiv@gmail.com>
2024-11-12 20:24:43 -0500

    feat: 557 complete

---
+++ 0557/0557.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseWords = function (s) {
+    return s
+        .split(' ')
+        .map((word) => word.split('').reverse().join(''))
+        .join(' ');
+};
