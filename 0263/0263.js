a76ceb9224cbf89c7ff597248d727300725af59a
William Higgins <higginsiv@gmail.com>
2024-11-20 21:09:48 -0500

    feat: 263 complete

---
+++ 0263/0263.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isUgly = function (n) {
+    if (n <= 0) {
+        return false;
+    }
+
+    while (n % 5 === 0) {
+        n /= 5;
+    }
+
+    while (n % 3 === 0) {
+        n /= 3;
+    }
+
+    while (n % 2 === 0) {
+        n /= 2;
+    }
+
+    return n === 1;
+};
