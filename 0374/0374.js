350f4274746844e902af12f1ae15b3f61230e391
William Higgins <higginsiv@gmail.com>
2025-02-05 18:36:01 -0500

    feat(374): complete

---
+++ 0374/0374.js
@@ -0,0 +1,36 @@
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const guessNumber = function (n) {
+    let low = 1;
+    let high = n;
+
+    while (high > low) {
+        const mid = Math.floor((high + low) / 2);
+        const result = guess(mid);
+
+        if (result === 0) {
+            return mid;
+        }
+
+        if (result === -1) {
+            high = mid - 1;
+        }
+
+        if (result === 1) {
+            low = mid + 1;
+        }
+    }
+
+    return low;
+};
